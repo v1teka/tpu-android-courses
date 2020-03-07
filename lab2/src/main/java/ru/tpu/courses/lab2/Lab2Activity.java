6566d4fffb58f3d7630a94890016512c5975cc56
v1teka <v1teka@yandex-team.ru>
2020-03-07 15:09:23 +0700

    Стабильный модуль 2 лабораторной

--- lab2/src/main/java/ru/tpu/courses/lab2/Lab2Activity.java
+++ lab2/src/main/java/ru/tpu/courses/lab2/Lab2Activity.java
@@ -8,36 +8,12 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-/**
- * <b>Вёрстка UI. Сохранение состояния.</b>
- * <p/>
- * Андроид старается минимизировать объём занимаемой оперативной памяти, поэтому при любом удобном
- * случае выгружает приложение или Activity из памяти. Например, при повороте экрана (если включен автоповорот),
- * весь объект Activity будет пересоздан с 0. Сохранить введенные данные можно несколькими способами:
- * <ul>
- * <li>Сохранить значения в оперативной памяти. Тогда данные переживут пересоздание Activity,
- * но не переживут освобождение приложения из памяти. Этот пример будет рассмотрен в 3ей лабораторной</li>
- * <li>Сохранить значения в файловой системе. Тогда данные переживут освобождение приложения.
- * Взаимодействие с файловой системой может быть длительной операцией и привносит свои проблемы.
- * Рассмотрено оно будет в 4ой лабораторной</li>
- * <li>Используя встроенную в андроид систему сохранения состояния, которую мы используем в
- * этой лабораторной работе. Перед уничтожением Activity будет вызван метод {@link #onSaveInstanceState(Bundle)},
- * в котором можно записать все необходимые значения в переданный объект Bundle. Стоит учитывать,
- * что дополнительно андроид сохраняет в него состояние всех View на экране по их идентификаторам.
- * Так, многие реализации View автоматически сохраняют свои данные, если им указан идентификатор.
- * Например, {@link android.widget.ScrollView} должен сохранять, на сколько отскроллен его
- * контент и восстанавливать его</li>
- * </ul>
- * <p/>
- */
 public class Lab2Activity extends AppCompatActivity {
-
     public static Intent newIntent(@NonNull Context context) {
         return new Intent(context, Lab2Activity.class);
     }
 
-    private static final String STATE_VIEWS_COUNT = "views_count";
-
+    private static final String STATE_VIEWS_VALUES = "viewsValues";
     private Lab2ViewsContainer lab2ViewsContainer;
 
     @Override
@@ -47,22 +23,23 @@ public class Lab2Activity extends AppCompatActivity {
 
         setTitle(getString(R.string.lab2_title, getClass().getSimpleName()));
 
-        // findViewById - generic метод https://docs.oracle.com/javase/tutorial/extra/generics/methods.html,
-        // который автоматически кастит (class cast) View в указанный класс.
-        // Тип вью, в которую происходит каст, не проверяется, поэтому если здесь указать View,
-        // отличную от View в XML, то приложение крашнется на вызове этого метода.
         lab2ViewsContainer = findViewById(R.id.container);
-        findViewById(R.id.btn_add_view).setOnClickListener(view -> lab2ViewsContainer.incrementViews());
 
-        // Восстанавливаем состояние нашего View, добавляя заново все View
+        findViewById(R.id.btn_add_view).setOnClickListener(view -> lab2ViewsContainer.addValue());
+        findViewById(R.id.btn_reset).setOnClickListener(view -> lab2ViewsContainer.initializeValues());
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
         if (savedInstanceState != null) {
-            lab2ViewsContainer.setViewsCount(savedInstanceState.getInt(STATE_VIEWS_COUNT));
+            lab2ViewsContainer.setViewsValues(savedInstanceState.getDoubleArray(STATE_VIEWS_VALUES));
         }
     }
 
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
-        outState.putInt(STATE_VIEWS_COUNT, lab2ViewsContainer.getViewsCount());
+        outState.putDoubleArray(STATE_VIEWS_VALUES, lab2ViewsContainer.getViewsValues());
     }
 }
--- lab2/src/main/java/ru/tpu/courses/lab2/Lab2ViewsContainer.java
+++ lab2/src/main/java/ru/tpu/courses/lab2/Lab2ViewsContainer.java
@@ -4,100 +4,95 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.os.Parcelable;
 import android.util.AttributeSet;
+import android.util.Range;
+import android.view.ContextThemeWrapper;
 import android.widget.LinearLayout;
+import 	android.widget.Toast;
 import android.widget.TextView;
 
 import androidx.annotation.Px;
 
-/**
- * Простейший пример самописного View. В данном случае мы просто наследуемся от LinearLayout-а и
- * добавляем свою логику, но также есть возможность отнаследоваться от {@link android.view.ViewGroup},
- * если необходимо реализовать контейнер для View полностью с нуля, либо отнаследоваться от {@link android.view.View}.
- * <p/>
- * Здесь можно было бы добавить автоматическое сохранение и восстановление состояния, переопределив методы
- * {@link #onSaveInstanceState()} и {@link #onRestoreInstanceState(Parcelable)}.
- */
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Lab2ViewsContainer extends LinearLayout {
 
     private int minViewsCount;
-    private int viewsCount;
+    private List<Double> viewsValues;
 
-    /**
-     * Этот конструктор используется при создании View в коде.
-     */
     public Lab2ViewsContainer(Context context) {
         this(context, null);
     }
 
-    /**
-     * Этот конструктор выдывается при создании View из XML.
-     */
     public Lab2ViewsContainer(Context context, AttributeSet attrs) {
         this(context, attrs, 0);
     }
 
-    /**
-     * Конструктор, вызывается при инфлейте View, когда у View указан дополнительный стиль.
-     * Почитать про стили можно здесь https://developer.android.com/guide/topics/ui/look-and-feel/themes
-     *
-     * @param attrs атрибуты, указанные в XML. Стандартные android атрибуты обрабатываются внутри родительского класса.
-     *              Здесь необходимо только обработать наши атрибуты.
-     */
     public Lab2ViewsContainer(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        // Свои атрибуты описываются в файле res/values/attrs.xml
-        // Эта строчка объединяет возможные применённые к View стили
         TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.Lab2ViewsContainer, defStyleAttr, 0);
 
         minViewsCount = a.getInt(R.styleable.Lab2ViewsContainer_lab2_minViewsCount, 0);
         if (minViewsCount < 0) {
             throw new IllegalArgumentException("minViewsCount can't be less than 0");
         }
-
-        // Полученный TypedArray необходимо обязательно очистить.
         a.recycle();
 
-        setViewsCount(minViewsCount);
+        initializeValues();
     }
 
-    /**
-     * Программно создаём {@link TextView} и задаём его атрибуты, альтернативно можно описать его в
-     * xml файле и инфлейтить его через класс LayoutInflater.
-     */
-    public void incrementViews() {
-        TextView textView = new TextView(getContext());
-        textView.setPadding(dpToPx(8), dpToPx(8), dpToPx(8), dpToPx(8));
-        textView.setTextSize(16);
-        textView.setText(String.valueOf(viewsCount++));
-        // У каждого View, который находится внутри ViewGroup есть LayoutParams,
-        // в них содержится информация для лэйаута компонентов.
-        // Базовая реализация LayoutParams содержит только определение ширины и высоты
-        // (то, что мы указываем в xml в атрибутах layout_widget и layout_height).
-        // Получить их можно через метод getLayoutParams у View. Метод addView смотрит, если у View
-        // не установлены LayoutParams, то создаёт дефолтные, вызывая метод generateDefaultLayoutParams
-        addView(textView);
+
+    public void initializeValues(){
+        setViewsValues(initialValues(minViewsCount));
     }
 
-    public void setViewsCount(int viewsCount) {
-        if (this.viewsCount == viewsCount) {
-            return;
-        }
-        viewsCount = viewsCount < minViewsCount ? minViewsCount : viewsCount;
+    private Random rand = new Random();
+    private double randomValue(){
+        return Math.round(rand.nextDouble()*100)/10.;
+    }
 
+    private double[] initialValues(int count){
+        double[] initialValues = new double[count];
+        for(int i=0;i<count;i++)
+            initialValues[i] = randomValue();
+
+        return initialValues;
+    }
+
+    public void addValue() {
+        double val = randomValue();
+        viewsValues.add(val);
+        addViewValue(val, viewsValues.size());
+//        Toast toast = Toast.makeText(getContext(),
+//                String.valueOf(val), Toast.LENGTH_SHORT);
+//        toast.show();
+    }
+
+    public void addViewValue(double viewValue, int number) {
+        Lab2View lab2View = new Lab2View(getContext());
+        lab2View.setValue(viewValue);
+        lab2View.setTitle("Запись номер "+number);
+        addView(lab2View);
+    }
+
+    public void setViewsValues(double[] values) {
         removeAllViews();
-        this.viewsCount = 0;
-        for (int i = 0; i < viewsCount; i++) {
-            incrementViews();
+        viewsValues = new ArrayList<>();
+        for (int i = 0; i < values.length; i++) {
+            viewsValues.add(values[i]);
+            addViewValue(values[i], i+1);
         }
     }
 
-    public int getViewsCount() {
-        return viewsCount;
+    public double[] getViewsValues() {
+        double[] target = new double[viewsValues.size()];
+        for (int i = 0; i < target.length; i++) {
+            target[i] = viewsValues.get(i);
+        }
+        return target;
     }
 
-    /**
-     * Метод трансформирует указанные dp в пиксели, используя density экрана.
-     */
     @Px
     public int dpToPx(float dp) {
         if (dp == 0) {
